be0bdc9c471385bc1b62eadbfda676aab05e6138
haeunia <peanutanna@ewhain.net>
2025-05-02 03:54:23 +0900

    week3 mission

---
+++ week3-2nd-mission/src/components/layout.tsx
@@ -0,0 +1,14 @@
+// src/components/Layout.tsx
+import Navbar from "./Navbar"
+import { Outlet } from "react-router-dom"
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <Navbar />
+      <main className="p-4">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
---
+++ week3-2nd-mission/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+// src/pages/Home.tsx
+export default function Home() {
+    return <h1 className="text-2xl font-bold">홈 페이지</h1>
+  }
+  
